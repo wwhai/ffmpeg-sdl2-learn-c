fbe74ed8c260a18f04e0522d9722c1ad1c37cb96
wwhai <cnwwhai@gmail.com>
2024-03-29 17:50:10 +0800

    dev: update

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int AddWaterMark(AVFrame *frame_in, AVFrame *frame_out, int w, int h, const char
     filter_graph = avfilter_graph_alloc();
     if (!filter_graph)
     {
-        fprintf(stderr, "Failed to allocate filter graph\n");
+        log_error("Failed to allocate filter graph");
         return AVERROR(ENOMEM);
     }
 
@@ -34,7 +34,7 @@ int AddWaterMark(AVFrame *frame_in, AVFrame *frame_out, int w, int h, const char
     buffersink = avfilter_get_by_name("buffersink");
     if (!buffersrc || !buffersink)
     {
-        fprintf(stderr, "Failed to get buffer source/sink filter\n");
+        log_error("Failed to get buffer source/sink filter");
         avfilter_graph_free(&filter_graph);
         return AVERROR_UNKNOWN;
     }
@@ -43,7 +43,7 @@ int AddWaterMark(AVFrame *frame_in, AVFrame *frame_out, int w, int h, const char
     ret = avfilter_graph_create_filter(&buffersrc_ctx, buffersrc, "in", NULL, NULL, filter_graph);
     if (ret < 0)
     {
-        fprintf(stderr, "Failed to create buffer source filter\n");
+        log_error("Failed to create buffer source filter");
         avfilter_graph_free(&filter_graph);
         return ret;
     }
@@ -52,7 +52,7 @@ int AddWaterMark(AVFrame *frame_in, AVFrame *frame_out, int w, int h, const char
     ret = avfilter_graph_create_filter(&buffersink_ctx, buffersink, "out", NULL, NULL, filter_graph);
     if (ret < 0)
     {
-        fprintf(stderr, "Failed to create buffer sink filter\n");
+        log_error("Failed to create buffer sink filter");
         avfilter_graph_free(&filter_graph);
         return ret;
     }
@@ -68,7 +68,7 @@ int AddWaterMark(AVFrame *frame_in, AVFrame *frame_out, int w, int h, const char
     const AVFilter *drawtext = avfilter_get_by_name("drawtext");
     if (!drawtext)
     {
-        fprintf(stderr, "Failed to get drawtext filter\n");
+        log_error("Failed to get drawtext filter");
         avfilter_graph_free(&filter_graph);
         return AVERROR_UNKNOWN;
     }
@@ -77,7 +77,7 @@ int AddWaterMark(AVFrame *frame_in, AVFrame *frame_out, int w, int h, const char
     ret = avfilter_graph_create_filter(&drawtext_ctx, drawtext, "drawtext", args, NULL, filter_graph);
     if (ret < 0)
     {
-        fprintf(stderr, "Failed to create drawtext filter\n");
+        log_error("Failed to create drawtext filter");
         avfilter_graph_free(&filter_graph);
         return ret;
     }
@@ -86,7 +86,7 @@ int AddWaterMark(AVFrame *frame_in, AVFrame *frame_out, int w, int h, const char
     ret = avfilter_link(buffersrc_ctx, 0, drawtext_ctx, 0);
     if (ret < 0)
     {
-        fprintf(stderr, "Failed to link buffersrc and drawtext filters\n");
+        log_error("Failed to link buffersrc and drawtext filters");
         avfilter_graph_free(&filter_graph);
         return ret;
     }
@@ -94,7 +94,7 @@ int AddWaterMark(AVFrame *frame_in, AVFrame *frame_out, int w, int h, const char
     ret = avfilter_link(drawtext_ctx, 0, buffersink_ctx, 0);
     if (ret < 0)
     {
-        fprintf(stderr, "Failed to link drawtext and buffersink filters\n");
+        log_error("Failed to link drawtext and buffersink filters");
         avfilter_graph_free(&filter_graph);
         return ret;
     }
@@ -103,7 +103,7 @@ int AddWaterMark(AVFrame *frame_in, AVFrame *frame_out, int w, int h, const char
     ret = avfilter_graph_config(filter_graph, NULL);
     if (ret < 0)
     {
-        fprintf(stderr, "Failed to configure filter graph\n");
+        log_error("Failed to configure filter graph");
         avfilter_graph_free(&filter_graph);
         return ret;
     }
@@ -112,7 +112,7 @@ int AddWaterMark(AVFrame *frame_in, AVFrame *frame_out, int w, int h, const char
     ret = av_buffersrc_add_frame_flags(buffersrc_ctx, frame_in, AV_BUFFERSRC_FLAG_KEEP_REF);
     if (ret < 0)
     {
-        fprintf(stderr, "Failed to send frame to filter graph\n");
+        log_error("Failed to send frame to filter graph");
         avfilter_graph_free(&filter_graph);
         return ret;
     }
@@ -121,7 +121,7 @@ int AddWaterMark(AVFrame *frame_in, AVFrame *frame_out, int w, int h, const char
     AVFrame *filtered_frame = av_frame_alloc();
     if (!filtered_frame)
     {
-        fprintf(stderr, "Failed to allocate filtered frame\n");
+        log_error("Failed to allocate filtered frame");
         avfilter_graph_free(&filter_graph);
         return AVERROR(ENOMEM);
     }
@@ -129,7 +129,7 @@ int AddWaterMark(AVFrame *frame_in, AVFrame *frame_out, int w, int h, const char
     ret = av_buffersink_get_frame(buffersink_ctx, filtered_frame);
     if (ret < 0)
     {
-        fprintf(stderr, "Failed to get filtered frame from filter graph\n");
+        log_error("Failed to get filtered frame from filter graph");
         av_frame_free(&filtered_frame);
         avfilter_graph_free(&filter_graph);
         return ret;
@@ -141,7 +141,7 @@ int AddWaterMark(AVFrame *frame_in, AVFrame *frame_out, int w, int h, const char
                                                 SWS_BILINEAR, NULL, NULL, NULL);
     if (!sws_ctx)
     {
-        fprintf(stderr, "Failed to initialize scaling context\n");
+        log_error("Failed to initialize scaling context");
         av_frame_free(&filtered_frame);
         avfilter_graph_free(&filter_graph);
         return AVERROR_UNKNOWN;
@@ -173,14 +173,14 @@ int main()
     ret = avformat_open_input(&input_ctx, input_url, NULL, NULL);
     if (ret < 0)
     {
-        fprintf(stderr, "Failed to open input RTSP stream: %s\n", av_err2str(ret));
+        log_error("Failed to open input RTSP stream: %s", av_err2str(ret));
         return ret;
     }
 
     ret = avformat_find_stream_info(input_ctx, NULL);
     if (ret < 0)
     {
-        fprintf(stderr, "Failed to retrieve input stream information: %s\n", av_err2str(ret));
+        log_error("Failed to retrieve input stream information: %s", av_err2str(ret));
         avformat_close_input(&input_ctx);
         return ret;
     }
@@ -189,7 +189,7 @@ int main()
     ret = avformat_alloc_output_context2(&output_ctx, NULL, "flv", output_url);
     if (ret < 0)
     {
-        fprintf(stderr, "Failed to create output context: %s\n", av_err2str(ret));
+        log_error("Failed to create output context: %s", av_err2str(ret));
         avformat_close_input(&input_ctx);
         return ret;
     }
@@ -200,7 +200,7 @@ int main()
         AVStream *out_stream = avformat_new_stream(output_ctx, NULL);
         if (!out_stream)
         {
-            fprintf(stderr, "Failed to create output stream\n");
+            log_error("Failed to create output stream");
             avformat_close_input(&input_ctx);
             avformat_free_context(output_ctx);
             return -1;
@@ -208,7 +208,7 @@ int main()
         ret = avcodec_parameters_copy(out_stream->codecpar, in_stream->codecpar);
         if (ret < 0)
         {
-            fprintf(stderr, "Failed to copy codec parameters: %s\n", av_err2str(ret));
+            log_error("Failed to copy codec parameters: %s", av_err2str(ret));
             avformat_close_input(&input_ctx);
             avformat_free_context(output_ctx);
             return ret;
@@ -220,7 +220,7 @@ int main()
         ret = avio_open(&output_ctx->pb, output_url, AVIO_FLAG_WRITE);
         if (ret < 0)
         {
-            fprintf(stderr, "Failed to open output URL: %s\n", av_err2str(ret));
+            log_error("Failed to open output URL: %s", av_err2str(ret));
             avformat_close_input(&input_ctx);
             avformat_free_context(output_ctx);
             return ret;
@@ -230,7 +230,7 @@ int main()
     ret = avformat_write_header(output_ctx, NULL);
     if (ret < 0)
     {
-        fprintf(stderr, "Failed to write header to output stream: %s\n", av_err2str(ret));
+        log_error("Failed to write header to output stream: %s", av_err2str(ret));
         avformat_close_input(&input_ctx);
         avformat_free_context(output_ctx);
         return ret;
@@ -240,7 +240,7 @@ int main()
     codec = avcodec_find_decoder(AV_CODEC_ID_H264); // 以H.264解码器为例
     if (!codec)
     {
-        fprintf(stderr, "Codec not found\n");
+        log_error("Codec not found");
         return -1;
     }
     AVCodecContext *codec_ctx = NULL;
@@ -248,7 +248,7 @@ int main()
     codec_ctx = avcodec_alloc_context3(codec);
     if (!codec_ctx)
     {
-        fprintf(stderr, "Could not allocate codec context\n");
+        log_error("Could not allocate codec context");
         return -1;
     }
     // 3. 打开解码器
@@ -256,47 +256,20 @@ int main()
     ret = avcodec_open2(codec_ctx, codec, NULL);
     if (ret < 0)
     {
-        fprintf(stderr, "Could not open codec: %s\n", av_err2str(ret));
+        log_error("Could not open codec: %s", av_err2str(ret));
         avcodec_free_context(&codec_ctx);
         return ret;
     }
-    //---
+
     AVPacket OriginPacket;
-    AVFrame DecodedFrame;
-    //---
     while (1)
     {
-        printf("1(0)\n");
-        // 接收解码后的帧
-        ret = avcodec_receive_frame(codec_ctx, &DecodedFrame);
-        if (ret < 0)
-        {
-            fprintf(stderr, "Error receiving frame from decoder: %s\n", av_err2str(ret));
-            av_packet_unref(&OriginPacket);
-            av_frame_unref(&DecodedFrame);
-            return ret;
-        }
-        printf("2(0)\n");
-
-        AVFrame FilteredFrame;
-        AVPacket NewPacket;
-        printf("AddWaterMark(0)\n");
-        AddWaterMark(&DecodedFrame, &FilteredFrame, 0, 0, "HELLO WORLD");
-        avcodec_send_frame(codec_ctx, &FilteredFrame);
-        while (avcodec_receive_packet(codec_ctx, &NewPacket) == 0)
-        {
-            av_packet_unref(&NewPacket);
-        }
-        av_frame_unref(&FilteredFrame);
-        //
-        //
-        //
         ret = av_read_frame(input_ctx, &OriginPacket);
         if (ret < 0)
         {
             if (ret == AVERROR_EOF)
                 break;
-            fprintf(stderr, "Error reading packet: %s\\n", av_err2str(ret));
+            log_error("Error reading packet: %s", av_err2str(ret));
             break;
         }
         AVStream *in_stream = input_ctx->streams[OriginPacket.stream_index];
@@ -307,19 +280,18 @@ int main()
         OriginPacket.dts = OriginPacket.pts;
 
         ret = av_interleaved_write_frame(output_ctx, &OriginPacket);
+        // printf("av_interleaved_write_frame: %d", OriginPacket.dts);
         if (ret < 0)
         {
-            fprintf(stderr, "Error writing packet: %s\n", av_err2str(ret));
-            goto final;
+            log_error("Error writing packet: %s", av_err2str(ret));
             break;
         }
-    final:
-        av_packet_unref(&OriginPacket);
     }
+    av_packet_unref(&OriginPacket);
     ret = av_write_trailer(output_ctx);
     if (ret < 0)
     {
-        fprintf(stderr, "Failed to write trailer to output stream: %s\n", av_err2str(ret));
+        log_error("Failed to write trailer to output stream: %s", av_err2str(ret));
         avformat_close_input(&input_ctx);
         avformat_free_context(output_ctx);
         return ret;
@@ -328,9 +300,10 @@ int main()
     // Cleanup
     avformat_close_input(&input_ctx);
     if (output_ctx && !(output_ctx->oformat->flags & AVFMT_NOFILE))
+    {
         avio_closep(&output_ctx->pb);
+    }
     avformat_free_context(output_ctx);
-
     avformat_network_deinit();
     return 0;
 }
